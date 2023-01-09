ab375abe7592342884b5a0108d49706d146b54ff
kkwock <kmaekwock@gmail.com>
2023-01-08 23:43:32 -0800

    Annotated Files

--- ncbi-ann-glyma.R
+++ ncbi-ann-glyma.R
@@ -1,5 +1,5 @@
 # Reading in the CSV file
-csv <- read.csv('data/FPKM_full-table.csv', header=T)
+# csv <- read.csv('data/FPKM_full-table.csv', header=T)
 
 # Function
 time <- function(){
@@ -16,49 +16,52 @@ geneSummary <- function(x){
   summ <- entrez_summary(db='gene', id=r_search$ids[1])
 }
 
-# Create a list of gene names from gene_name column
-gene_list <- csv$gene_name
-
-# for large dataset
-n <- 1000
-df <- data.frame()
-full.df <- data.frame()
-
-for(j in 1:ceiling(nrow(csv)/n)) {
-  nrange <- (n*(j-1)+1):(min(n*j, nrow(csv)))
-  time()
-  cat(paste0(j, "/", ceiling(nrow(csv)/n)))
-  cat("\nrange: ", range(nrange))
+annotate <- function(csv, output.name){
+  # Create a list of gene names from gene_name column
+  gene_list <- csv$gene_name
   
-  # Looping through each name in the gene list and creating a new dataframe    
-  for(i in nrange){
-    gene <- gene_list[i]
+  # for large dataset
+  n <- 1000
+  df <- data.frame()
+  full.df <- data.frame()
+  
+  for(j in 1:ceiling(nrow(csv)/n)) {
+    nrange <- (n*(j-1)+1):(min(n*j, nrow(csv)))
+    time()
+    cat(paste0(j, "/", ceiling(nrow(csv)/n)))
+    cat("\nrange: ", range(nrange))
     
-    # Progress Check
-    if(i/max(nrange)==.25){
-      time()
-      print("25%")
-    }else if(i/max(nrange)==.5){
-      time()
-      print("50%")
-    }else if(i/max(nrange)==.75){
-      time()
-      print("75%")
-    }else if(i/max(nrange)==1){
-      time()
-      print("100%")
+    # Looping through each name in the gene list and creating a new dataframe    
+    for(i in nrange){
+      gene <- gene_list[i]
+      
+      # Progress Check
+      if(i/max(nrange)==.25){
+        time()
+        print("25%")
+      }else if(i/max(nrange)==.5){
+        time()
+        print("50%")
+      }else if(i/max(nrange)==.75){
+        time()
+        print("75%")
+      }else if(i/max(nrange)==1){
+        time()
+        print("100%")
+      }
+      
+      if(gene == '.'){
+        temp <- data.frame('gene_name' = gene, 'glyma' = NA, 'desc' = NA)
+      }else{
+        summ <- geneSummary(gene)
+        temp <- data.frame('gene_name' = gene, 'glyma' = summ$otheraliases, 'desc' = summ$description)
+      }
+      df <- rbind(df, temp)
     }
+    full.df <- rbind(full.df, df)
     
-    if(gene == '.'){
-      temp <- data.frame('gene_name' = gene, 'glyma' = NA, 'desc' = NA)
-    }else{
-      summ <- geneSummary(gene)
-      temp <- data.frame('gene_name' = gene, 'glyma' = summ$otheraliases, 'desc' = summ$description)
-    }
-    df <- rbind(df, temp)
+    # Create CSV file of annotations
+    write.csv(full.df, output.name, row.names=F)
   }
-  full.df <- rbind(full.df, df)
-    
-  # Create CSV file of annotations
-  write.csv(full.df, 'data/Full_GlymaAnnotations.csv')
-  }
+  return(full.df)
+}
